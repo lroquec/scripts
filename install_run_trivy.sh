26218cd77ad33cfa674af220f9d6921ae78cdb93
Laura Roque <lroquecu@gmail.com>
2024-11-10 15:16:47 +0100

    trivy where to get updates

--- install_run_trivy.sh
+++ install_run_trivy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check for updates https://github.com/aquasecurity/trivy
+
+# Get the package
 wget https://github.com/aquasecurity/trivy/releases/download/v0.57.0/trivy_0.57.0_Linux-64bit.deb
+
+# Install the package
 sudo dpkg -i trivy_0.57.0_Linux-64bit.deb
+
+# Scan the image
 trivy image --severity CRITICAL --exit-code 0 --format sarif --output trivy-report.sarif lroquec/$app_name:latest
