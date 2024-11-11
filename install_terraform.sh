13aa42b89f7f43ac7d773c265419af429976af95
Laura Roque <lroquecu@gmail.com>
2024-11-11 09:23:13 +0100

    terraform

---
+++ install_terraform.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check updates from https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+
+# Install Terraform Ubuntu
+echo "Installing Terraform..."
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+terraform --version
+echo "Terraform installed successfully"
+
+# For Amazon Linux
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum -y install terraform
+
+# For autocomplete
+terraform -install-autocomplete
